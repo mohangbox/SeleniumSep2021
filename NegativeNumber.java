55ff7fb4e281934aa70fab29475df7af5aaedd35
mohangbox <mohangbox@gmail.com>
2021-09-05 08:56:26 +0530

    Add files via upload

---
+++ NegativeNumber.java
@@ -0,0 +1,15 @@
+package week1.day1Assignment;
+
+public class NegativeNumber {
+
+	public static void main(String[] args) {
+		int number = -40, num2;
+		if (number < 0) {
+			System.out.println("The number is negative number" + number);
+			num2 = number - (number * 2);
+			System.out.println("The positive number is " + num2);
+		}
+
+	}
+
+}
